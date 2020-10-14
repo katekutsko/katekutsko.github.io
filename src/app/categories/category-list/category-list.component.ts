0a53ffcf0ad69541091f2bcbb57aae69cf3badca
Katsiaryna_Kutsko <katsiaryna_Kutsko@epam,com>
2020-10-15 01:47:04 +0300

    refactoring

--- src/app/categories/category-list/category-list.component.ts
+++ src/app/categories/category-list/category-list.component.ts
@@ -1,16 +1,16 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 import { CategoryService } from '../category/category.service';
 import { ICategory } from '../category/icategory';
 
 @Component({
-    selector: 'categories-list',
-    templateUrl: './category-list.component.html',
-    styleUrls: ['./category-list.component.scss']
+  selector: 'categories-list',
+  templateUrl: './category-list.component.html',
+  styleUrls: ['./category-list.component.scss'],
 })
 export class CategoryListComponent {
-    categories : ICategory[];
+  categories: ICategory[];
 
-    constructor(categoriesService : CategoryService) {
-        this.categories = categoriesService.getCategories();
-    }
+  constructor(categoriesService: CategoryService) {
+    this.categories = categoriesService.getCategories();
+  }
 }
