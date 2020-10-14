6e299c0e71504f696700a908f6a547e6c2dea432
Katsiaryna_Kutsko <katsiaryna_Kutsko@epam,com>
2020-10-15 02:02:30 +0300

    chaged folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header.component';
-import { HomeComponent } from './home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { HomeComponent } from './home/home.component';
 import { CategoryListComponent } from './categories/category-list/category-list.component';
 import { CouponListComponent } from './coupons/coupon-list/coupon-list.component';
 import { CouponCardComponent } from './coupons/coupon/coupon.component';
