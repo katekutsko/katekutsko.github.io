418709de314c4d467a76d8553893177281e3ef31
Katsiaryna_Kutsko <katsiaryna_Kutsko@epam,com>
2020-10-23 14:15:18 +0300

    removed irrelevant code

--- src/app/modules/details/details-routing.module.ts
+++ src/app/modules/details/details-routing.module.ts
@@ -3,16 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { DetailsComponent } from './details.component';
 
-const routes: Routes = [
-  { path: '', component: DetailsComponent },
-  {
-    path: 'checkout',
-    loadChildren: () =>
-      import('../../modules/checkout/checkout.module').then(
-        (m) => m.CheckoutModule
-      ),
-  },
-];
+const routes: Routes = [{ path: '', component: DetailsComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
